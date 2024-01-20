699550b27eb36a7039d6af7b50efabc19548c8fa
Paulos Tesfazghi <71388869+pth2020@users.noreply.github.com>
2024-01-20 23:03:46 +0000

    Update data.py

--- GCSE maths quiz/data.py
+++ GCSE maths quiz/data.py
@@ -1,1 +1,322 @@
+#################################
+#Question bank and solutions
 
+question_bank = {
+    'number':{
+        'percentages': {
+            '1':{
+                'What is 45% of 120?':['54','64','24','44']
+            },           
+            '2':{
+                'What is 120 increased by 25%?':['40','140','180','150']
+            },
+            '3':{
+                'A car\'s value depreciated by 15%. Now it\'s £8500 worth.\n' +
+                'What was it worth originally?':
+                ['£12,000','£10,000','£9,500','£15,000']
+            },
+            '4':{
+                'A doctor has 12,000 patients. 4560 of these paitients are male.\n' +
+                'What percentage of these patients are females?':
+                ['38%','56.5%','62%','40%']           
+            },
+            '5':{
+                'A school had 5500 students in 2022. In 2023 the number increased to 6050.\n' + 
+                'What is the percentage increase? ':
+                ['15%','12%','10%','20%']
+            }
+        },
+        'fractions':{
+            '1':{
+                'What is 2/3 of 135?':['75','120','45','90']
+            },           
+            '2':{
+                'Put these fractions in order from smallest to largest. 4/5, 1/2, 5/6, 29/30': 
+                ['4/5  5/6  1/2  29/30','1/2  4/5  5/6  29/30','5/6  1/2  4/5   29/30 ','29/30  5/6  4/5  1/2']
+            },
+            '3':{
+                'James earns £800 a month. He spends 1/4 on rent and 2/5 on food and bills. \n' + 
+                'How much money has he left?':
+                ['£280','£620','£360','£440']
+            },
+            '4':{
+                'Hannah has £900. She spends 1/3 on books and 2/5 on presents. \n' + 
+                'What fraction of the £900 has she left?':
+                ['11/15','4/15','3/5','1/6']           
+            },
+            '5':{
+                'In January a baby elephant weighs 180kg. By March its weight increased by 3/8.\n' + 'Work out its weight in March ':
+                ['195.5kg','67.6kg','245.5kg','247.5kg']
+            }
+            
+        },
+        'decimals':{
+            '1':{
+                'Work out 16.65 ÷ 0.3 (No calculator)':['18.9','65.2','45.6','55.5']
+            },           
+            '2':{
+                'Given this information 2.56 x 3.6 = 9.216 , work out 256 x 36.': 
+                ['92.16','921.6','9216','92160']
+            },
+            '3':{
+                'Mrs Webb has a roll of ribbon that is 44.1m long. \n' + 
+                'She needs strips of ribbon that are 2.5m long. \n' + 
+                'How many 2.5m strips will Mrs Webb get from the roll?':
+                    ['17','18','20','25']
+            },
+            '4':{
+                'Put these decimals in order from smallest to largets.\n' +
+                '0.67  0.128   0.6   0.045':
+                ['0.6  0.67  0.045  0.128','0.045  0.6  0.67  0.128','0.045  0.128  0.6  0.67','0.67  0.6  0.128  0.045']           
+            },
+            '5':{
+                'Sam paid £26.25 for three books that all cost the same amount.\n ' +
+                'What was the cost per book?':
+                ['7.85','£13.13','£5.25','£8.75']
+            }
+        },
+        'standard_form':{
+                '1':{
+                    'What is 8200000 in standard form':
+                    ['82 x 10^5','8.2 x 10^6','0.82 x 10^7','820 x 10^4']
+                },           
+                '2':{
+                    'What is 0.045 in standard form?': 
+                    ['4.5 x 10^-2','4.5 x 10^2','0.45 x 10^-1','45 x 10^-3']
+                },
+                '3':{
+                    'What is 5.6 x 10^3 as an ordinary number?':
+                    ['560','56','0.0056','5600']
+            },
+            '4':{
+                'Work out (5 × 10^6) × (7 × 10^8) and write in standard form.':
+                ['35 x 10^14','3.5 x 10^15','0.35 x 10^16','3.5x10^49']           
+            },
+            '5':{
+                'Work out (2.1 × 10^−5) ÷ (7 × 10^−4) and write in standard form.':
+                ['3 x 10^-1','3 x 10^-3','3 x 10^-2','3']
+            }
+        },
+        'compound_interest':{
+            '1':{
+                    'Which of these is a compound interest formula?':
+                    ['A=P(1-r/100)^n','A=P(1+r/100)^n','A=n(1+p/100)^r','A=r(1+n/100)^p']
+             },           
+            '2':{
+                    'Fiona leaves £1600 in the bank for four years. \n' + 
+                    'It earns compound interest of 4% each year.\n' +
+                    'Calculate the total amount Fiona has in the bank at the end of the four years': 
+                    ['£1871.77','£6146.56','£1963.67','£2100.35']
+             },
+            '3':{
+                    'Natalie invests £600 for 2 years at 10% per year compound interest.\n' +
+                    'How much interest does she earn?':
+                    ['£726','£126','£12.06','£612.06']
+            },
+            '4':{
+                '£5200 is invested at 2.8% compound interest per annum.\n' +
+                'How many years will it take for the investment to exceed £7000?':
+                ['10 years','12 years','11 years','8 years']           
+            },
+            '5':{
+                'Find the final amount when £25,000 is invested at 1.2% compound \n' + 
+                'interest per month for 3 years.':
+                ['£38,409.48','25,910.84','£31,458.23','£42,490.01']
+            }
+        }
+    },
+    'algebra':{
+        'simplify_expressions': {
+            '1':{
+                'Simplified form of this expression:  8c + 2p − 10c + 4p':
+                ['2c + 6p','-2c + 6p','2c + 2p','10p - 6c']
+            },
+            '2':{
+                'Simplified form of this expression: 3c − 10d − c + 4d':
+                ['2c + 6d','2c + 14d','7c + 5d','2c - 6d']
+            },
+            '3':{
+                'A rectangle has sides (2x + 7) and (x + 3).\n' +
+                'Which expression shows perimeter of the rectangle?' :
+                ['6x + 20','3x + 10','12x + 40','9x + 30']
+            },
+            '4':{
+                'Ann is x years old. \nBob is twice as old as Ann.\n' +  
+                'Claire is 3 years younger than Bob.\n' 
+                'Which expression shows the total ages of Ann, Bob and Claire?':
+                ['10x - 3','5x - 3','3x - 3','5x + 3']                      
+            },
+            '5':{
+                'The Number 30 bus can carry x people. \n' +
+                'The Number 25 carries 8 less people than the Number 30.\n' +
+                'The Number 8 carries 2 times as many people as the Number 25.\n' +
+                'Which expression shows the number of people the number 8 bus carries?':
+                ['x - 8','x - 16','2x - 8','2x - 16']
+            }
+        },
+        'substitution':{
+            '1':{
+                'What is the value of this expression (4a - 6) when a = 3?':['6','8','-6','10']
+            },           
+            '2':{
+                'What is the value of this expression (2x - y) when x = 3 and y = 8?': 
+                ['13','-2','19','21']
+            },
+            '3':{
+                'This formula can be used to convert between Celsuis (C) and Fahrenheit (F)' + 
+                'F = 1.8C + 32' + 
+                'What is 2 degree C':
+                ['33.8','37.4','35.6','40.5']
+            },
+            '4':{
+                'What is the value of this expression  (ab + 24)/2c  when a = 4, b = 9 and c = -5?':
+                ['-6','-12','8','4']           
+            },
+            '5':{
+                'P = 2W + 2L\n' + 
+                'What is W if P = 30 and L = 11':
+                ['8','4','88','16']
+            }
+            
+        },
+        'solve_equations':{
+            '1':{
+                'Solve for x :  2x + 8 = 12 ':
+                ['x = 8','x = 2','x = 4','x = 6']
+            },           
+            '2':{
+                'Solve for x: 3x - 5 = 5x - 15 ':
+                ['x = 10','x = 5','x = 4','x = 2.5']
+            },
+            '3':{
+                'Solve for x:   (3x - 4)/10  = 2':
+                    ['x = 3','x = 10','x = 6','x = 8']
+            },
+            '4':{
+                'The length of a rectangle is twice as long as its width.\n' + 
+                'The area of the rectangle is 128cm sq.' +
+                'What is the length of the rectangle?':
+                ['16cm','12cm','30cm','24cm']           
+            },
+            '5':{
+                'Mr Thomas bought several gift cards for his friends.\n' +
+                'Each gift card was valued at £18.\n' +
+                'He also bought a perfume for his wife for the price of £10.\n' +
+                'He spent £100 altogether.\n' + 
+                'How many gift cards did he buy?':
+                    ['4','12','5','6']
+            }
+        },
+        'expand_and_factorise':{
+            '1':{
+                'Expand:  3(x - 6)':
+                    ['3x - 6','3x - 9','3x - 18','x - 18']
+            },           
+            '2':{
+                'Expand: 4x(2x - 5)': 
+                ['8x - 20','8x^2 - 20x','6x - 20','6x^2 - 9']
+            },
+            '3':{
+                'Factorise fully: 16x + 24':
+                 ['16(x + 4)','4(x + 6)','2(8x + 12)','4(4x + 6)']
+            },
+            '4':{
+                'Factorise fully:  9ab + 27b':
+                ['3b(3a + 9)','9b(a + 3)','b(9a + 27)','6b(9a + 27)']           
+            },
+            '5':{
+                'Expand:  (2x - 1)(3x + 5)':
+                ['6x^2 - 7x + 5','12x^2 + 5x - 5','5x^2 + 7x - 5','6x^2 + 7x - 5']
+            }
+        },
+        'linear_sequences':{
+            '1':{
+                'What is the zeroth (z) term of this linear sequence?\n' +
+                '5,9,13,17,...':
+                ['4','1','-1','5']
+            },           
+            '2':{
+                'What is the difference (d) between each term?\n' + 
+                '6,13,20,27,34,...' :                
+                ['8','7','9','12']
+            },
+            '3':{
+                'What is the formula to find the nth term of a linear sequence':
+                ['nth = d x n + z','nth = z x n + d','nth = n + d x z','nth = n x z x d']
+            },
+            '4':{
+                'Find the nth term of this sequence.\n' + 
+                '7,11,15,19,23,...':
+                ['12n','n + 12','3n + 4','4n + 3']          
+            },
+            '5':{
+                'Find the 20th term of this sequence.\n' + 
+                '20,14,8,2,...': 
+                ['-120','-94','-64','-88']
+            }
+        }
+    }
+}  
+answers = {
+    'number_percentages_Q1':'a', 
+    'number_percentages_Q2':'d',
+    'number_percentages_Q3':'b', 
+    'number_percentages_Q4':'c',
+    'number_percentages_Q5':'c', 
+
+    'number_fractions_Q1':'d', 
+    'number_fractions_Q2':'b',
+    'number_fractions_Q3':'a', 
+    'number_fractions_Q4':'b',
+    'number_fractions_Q5':'d', 
+
+    'number_decimals_Q1':'d', 
+    'number_decimals_Q2':'d',
+    'number_decimals_Q3':'a', 
+    'number_decimals_Q4':'d',
+    'number_decimals_Q5':'a', 
+
+    'number_standad_form_Q1':'b', 
+    'number_standad_form_Q2':'a',
+    'number_standad_form_Q3':'d', 
+    'number_standad_form_Q4':'b',
+    'number_standad_form_Q5':'c', 
+
+    'number_compound_interest_Q1':'b', 
+    'number_compound_interest_Q2':'a',
+    'number_compound_interest_Q3':'b', 
+    'number_compound_interest_Q4':'b',
+    'number_compound_interest_Q5':'a', 
+
+    'algebra_simplify_expressions_Q1':'a', 
+    'algebra_simplify_expressions_Q2':'d',
+    'algebra_simplify_expressions_Q3':'a', 
+    'algebra_simplify_expressions_Q4':'d',
+    'algebra_simplify_expressions_Q5':'a', 
+
+    'algebra_substitution_Q1':'a6', 
+    'algebra_substitution_Q2':'b',
+    'algebra_substitution_Q3':'c', 
+    'algebra_substitution_Q4':'a',
+    'algebra_substitution_Q5':'b', 
+
+    'algebra_solve_equations_Q1':'b', 
+    'algebra_solve_equations_Q2':'b',
+    'algebra_solve_equations_Q3':'d', 
+    'algebra_solve_equations_Q4':'a',
+    'algebra_solve_equations_Q5':'c', 
+
+    'algebra_expand_and_factorise_Q1':'c', 
+    'algebra_expand_and_factorise_Q2':'b',
+    'algebra_expand_and_factorise_Q3':'d', 
+    'algebra_expand_and_factorise_Q4':'b',
+    'algebra_expand_and_factorise_Q5':'d', 
+
+    'algebra_linear_sequences_Q1':'b', 
+    'algebra_linear_sequences_Q2':'b',
+    'algebra_linear_sequences_Q3':'a', 
+    'algebra_linear_sequences_Q4':'d',
+    'algebra_linear_sequences_Q5':'b'
+} 
+#End of Question bank and solutions
+####################################
